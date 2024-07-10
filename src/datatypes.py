5731e01fc1ced2d0d7628551db89b19b8b21444d
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-10 16:44:43 +0200

    Create datatypes.py

---
+++ src/datatypes.py
@@ -0,0 +1,13 @@
+# Datatypes are the different types of variables.
+# In python you define those like this:
+# Python has only 3 important Datatypes.
+
+name = 'John' # String-Datatype: Strings are used to safe text variables
+natural_number = 2 # Integer-Datatypes: Integers are used to safe numbers for mathematical operations. Integers are natural numbers.
+float_number = 2.25 # Float-Datatypes: Floats are used to safe decimal numbers. You have to set a . for the Comma.
+
+
+# Outputs
+print(name)
+print(natural_number)
+print(float_number)
