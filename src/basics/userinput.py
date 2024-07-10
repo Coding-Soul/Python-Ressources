22b75386b46f2f9a2cefc9c24f3b33d9c5ee78b8
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-10 20:44:53 +0200

    Create userinput.py

---
+++ src/basics/userinput.py
@@ -0,0 +1,6 @@
+# Important for creating automation scripts, where user can enter data is the input function.
+
+name = input('Enter your name: ') # Input Function
+age = input('Enter your age: ')
+
+print("Your name is " + name + " and you are " + age + " years old.") # printing the result
