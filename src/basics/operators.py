1de85b557edea90f95e585711cbe7604a5093c97
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-13 17:08:33 +0200

    Update operators.py

---
+++ src/basics/operators.py
@@ -0,0 +1,16 @@
+# We can use Operators, to calculate things:
+# In Python that is realy simple:
+
+number1 = 50
+number2 = 25
+
+added = number1 + number2 # Adding the numbers
+subtrahend = number1 - number2 # Subtracting the numbers
+multiplied = number1 * number2 # Multiplying the numbers
+divided = number1 / number2 # Dividing the numbers
+
+# Printing the result
+print(added)
+print(subtrahend)
+print(multiplied)
+print(divided)
