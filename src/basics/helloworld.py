4a071ec5691174f691c5080a4fed6206e0961462
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-14 11:36:00 +0200

    change: helloworld.py
    Improved spelling mistake

--- src/basics/helloworld.py
+++ src/basics/helloworld.py
@@ -1,3 +1,3 @@
-# A classic first project in a programming language is printing a simple "Hello World!" in the console.
+# A classical first project in a programming language is printing a simple "Hello World!" in the console.
 # Compared to other languages, is python verry easy in this function: You only need one single line of code!
 print("Hello World!") # The function print, will give an output!
